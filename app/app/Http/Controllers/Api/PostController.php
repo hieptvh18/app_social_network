21ecaf0ea79de642df8a2323ca4a354ce9cda277
dukeondaB <79830923+dukeondaB@users.noreply.github.com>
2023-06-26 00:16:03 +0700

    get list post

--- app/app/Http/Controllers/Api/PostController.php
+++ app/app/Http/Controllers/Api/PostController.php
@@ -3,39 +3,17 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Follow;
-use App\Models\Post;
 use App\Repositories\Interfaces\PostRepositoryInterface;
-use App\Repositories\PostRepository;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 
 class PostController extends Controller
 {
     protected $postRepository;
-    public function __contruct(PostRepositoryInterface $postRepositoryInterface){
+    public function __construct(PostRepositoryInterface $postRepositoryInterface){
        $this->postRepository = $postRepositoryInterface;
     }
     public function getPostByFollowingId(){
-        // $followingId = Auth::id();
-        // $followingList = Follow::where('following_id',$followingId)->get('user_id');
-        // $array = $followingList->map(function ($item) {
-        //     return $item->getAttributes();
-        // })->all();
 
-        // $posts = Post::whereIn('user_id',$array)->get();
-        // if ($posts) {
-        //     return response()->json([
-        //         'data' => $posts,
-        //         'message' => 'Get post success',
-        //         'success' => true,
-        //     ]);
-        // }
-        // return response()->json([
-        //     'data' => [],
-        //     'message' => 'Post not found!',
-        //     'success' => false,
-        // ]);
-            return $this->postRepository->getPostByFollowingId();
+        return $this->postRepository->getPostByFollowingId();
     }
 }
