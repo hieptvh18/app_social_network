38cd8e7e562d1a462bfea5a50b42f4798f9be5a7
hieptvh18 <hieptvh18@gmail.com>
2023-07-06 16:56:45 +0700

    fix namspace controller

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\api\PostController;
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
